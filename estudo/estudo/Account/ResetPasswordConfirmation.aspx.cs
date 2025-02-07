1f663d3a7de25d0b73c60b236af5ed5281fda8ff
fabio <fabioferreiraghetler@outlook.com>
2025-02-07 15:43:41 -0500

    My personal web site

---
+++ estudo/estudo/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace estudo.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ estudo/estudo/html/js/functions.js
@@ -0,0 +1,7 @@
+﻿function mudaFoto(foto) {
+    document.getElementById("icone").src = foto;
+}
+
+//nesta função, nos pegamos o elemento que está dentro do conteiner com o id "icone"
+//e recebemos o que estiver contido dentro do paramentro "foto"
+//lá no html vemos que o ID "icone" está associado ao "a.jpg"
